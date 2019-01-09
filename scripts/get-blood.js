bcf21f2a7dc103ece7db7b99bea0ad661f244415
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-01-09 06:04:36 +0530

    MapBox Edited

--- scripts/get-blood.js
+++ scripts/get-blood.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGVzdGluZ2d1eSIsImEiOiJjanFvNTFsd2kwOTN3NDJwc
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v9',
-  center: [77.200, 28.7041],
+  center: [77.2169, 28.6673],
   zoom: 11.25
 });
 
@@ -18,7 +18,7 @@ map.on('load', function () {
             "features": [{
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+                    "description": "Kohat Enclave",
                     "icon": "star"
                 },
                 "geometry": {
@@ -28,82 +28,82 @@ map.on('load', function () {
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href=\"http://madmens5finale.eventbrite.com/\" target=\"_blank\" title=\"Opens in a new window\">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>",
+                    "description": "Prashant Vihar",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1320, 28.6790]
+                    "coordinates": [77.1330, 28.7141]
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href=\"http://tallulaeatbar.ticketleap.com/2012beachblanket/\" target=\"_blank\" title=\"Opens in a new window\">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>",
+                    "description": "Shalimar Bagh",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1400, 28.6890]
+                    "coordinates": [77.1696, 28.7034]
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Ballston Arts & Crafts Market</strong><p>The <a href=\"http://ballstonarts-craftsmarket.blogspot.com/\" target=\"_blank\" title=\"Opens in a new window\">Ballston Arts & Crafts Market</a> sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>",
+                    "description": "Karol Bagh",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1300, 28.6910]
+                    "coordinates": [77.1912, 28.6527]
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's <a href=\"http://dandiesandquaintrelles.com/2012/04/the-seersucker-social-is-set-for-june-9th-save-the-date-and-start-planning-your-look/\" target=\"_blank\" title=\"Opens in a new window\">Seersucker Social</a> bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>",
+                    "description": "Cannaught Bagh",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1384, 28.6910]
+                    "coordinates": [77.2080, 28.6307]
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Capital Pride Parade</strong><p>The annual <a href=\"http://www.capitalpride.org/parade\" target=\"_blank\" title=\"Opens in a new window\">Capital Pride Parade</a> makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>",
+                    "description": "Geeta Colony",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1384, 28.6790] //
+                    "coordinates": [77.2724, 28.6526] 
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Muhsinah</strong><p>Jazz-influenced hip hop artist <a href=\"http://www.muhsinah.com\" target=\"_blank\" title=\"Opens in a new window\">Muhsinah</a> plays the <a href=\"http://www.blackcatdc.com\">Black Cat</a> (1811 14th Street NW) tonight with <a href=\"http://www.exitclov.com\" target=\"_blank\" title=\"Opens in a new window\">Exit Clov</a> and <a href=\"http://godsilla.bandcamp.com\" target=\"_blank\" title=\"Opens in a new window\">Gods’illa</a>. 9:00 p.m. $12.</p>",
+                    "description": "Yamuna Vihar",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1484, 28.6990] //
+                    "coordinates": [77.2681, 28.7023] 
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>A Little Night Music</strong><p>The Arlington Players' production of Stephen Sondheim's  <a href=\"http://www.thearlingtonplayers.org/drupal-6.20/node/4661/show\" target=\"_blank\" title=\"Opens in a new window\"><em>A Little Night Music</em></a> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>",
+                    "description": "Rajouri Garden",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [77.1399, 28.6921]
+                    "coordinates": [77.1234, 28.6474]
                 }
             }, {
                 "type": "Feature",
                 "properties": {
-                    "description": "<strong>Truckeroo</strong><p><a href=\"http://www.truckeroodc.com/www/\" target=\"_blank\">Truckeroo</a> brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>",
+                    "description": "Mayur Vihar",
                     "icon": "star"
                 },
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [78.9324, 28.1910]
+                    "coordinates": [77.2946, 28.6088]
                 }
             }]
         }
@@ -115,15 +115,15 @@ map.on('load', function () {
   });
 });
 
-const cordi = [[77.1384, 28.6990],
-               [77.1320, 28.6790],
-               [77.1400, 28.6890],
-               [77.1300, 28.6910],
-               [77.1384, 28.6910],
-               [77.1384, 28.6790],
-               [77.1484, 28.6990],
-               [77.1399, 28.6921],
-               [77.9324, 28.5910]
+const cordi = [[77.1384, 28.6990, 'Kohat Enclave' ],
+               [77.1330, 28.7141, 'Prashant Vihar'],
+               [77.1696, 28.7034, 'Shalimar Bagh'],
+               [77.1912, 28.6527, 'Karol Bagh'],
+               [77.2080, 28.6307, 'Cannaught Place'],
+               [77.2724, 28.6526, 'Geeta Colony'],
+               [77.2681, 28.7023, 'Yamuna Vihar'],
+               [77.1234, 28.6474, 'Rajouri Garden'],
+               [77.2946, 28.6088, 'Mayur Vihar']
               ]; 
             
 var para = document.getElementById("demo");
@@ -141,8 +141,8 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    currentLoc.push(position.coords.latitude)
     currentLoc.push(position.coords.longitude);
+    currentLoc.push(position.coords.latitude)
     KNN(currentLoc);
 }
 
@@ -173,10 +173,47 @@ function indexOfSmallest(a) {
 };
 
 function showLocOfNearest(lowest){
-  para.innerHTML = "Latitude: " + cordi[lowest][0] + "<br>Longitude: " + cordi[lowest];
+  para.innerHTML = `Location: ${cordi[lowest][2]} <br>
+                    Longitude: ${cordi[lowest][0]} <br> 
+                    Latitude: ${cordi[lowest][1]}`; 
 }
 
+// function newMaprender(){
+//   mapboxgl.accessToken = 'pk.eyJ1IjoidGVzdGluZ2d1eSIsImEiOiJjanFvNTFsd2kwOTN3NDJwcHg1dGhuMDIxIn0.N3GDeGA8KOSp0ZbZ9262hA';
 
+// var map1 = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v9',
+//   center: [77.200, 28.7041],
+//   zoom: 11.25
+// });
 
+// map1.on('load', function () {
+//   map.addLayer({
+//     "id": "new",
+//     "type": "symbol",
+//     "source": {
+//         "type": "geojson",
+//         "data": {
+//             "type": "FeatureCollection",
+//             "features": [{
+//                 "type": "Feature",
+//                 "properties": {
+//                     "description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+//                     "icon": "star"
+//                 },
+//                 "geometry": {
+//                     "type": "Point",
+//                     "coordinates": [cordi[lowest][1],cordi[lowest][0]]
+//                 }
+//             }]
+//           }
+//         },
+//     "layout": {
+//         "icon-image": "{icon}-15",
+//         "icon-allow-overlap": true
+//     }
+//   });
+// });
 
-
+// }
